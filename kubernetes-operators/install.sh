0122c216bc98833281402179d08e66f489a8247b
zeldi suryady <zeldi.kamalurradat@tmrnd.com.my>
2023-11-17 09:55:52 +0800

    install kubebuilder

---
+++ kubernetes-operators/install.sh
@@ -0,0 +1,12 @@
+curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)
+chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
+
+# update go
+wget https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
+go version
+rm go1.19.3.linux-amd64.tar.gz
+
+# make working directory
+mkdir -p src
+cd ./src
