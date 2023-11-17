2dec3a59c06117f9d458892f6ebdb1cfffd0ce83
zeldi suryady <zeldi.kamalurradat@tmrnd.com.my>
2023-11-18 07:51:31 +0800

    updated installer.sh

--- kubernetes-operators/install.sh
+++ kubernetes-operators/install.sh
@@ -1,13 +1,22 @@
-curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)
+version=v3.13.0
+curl -L -o kubebuilder https://go.kubebuilder.io/dl/${version}/$(go env GOOS)/$(go env GOARCH)
 chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
 
 echo "Install/Update go"
-wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.20.11.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.11.linux-amd64.tar.gz
 go version
-rm go1.21.4.linux-amd64.tar.gz
+rm go1.20.11.linux-amd64.tar.gz
+
 
 echo "make working directory"
 mkdir -p src
 cd ./src
 clear
+
+echo "Creating GOPATH directory"
+mkdir -p /opt/go/{bin,pkg,src};
+echo 'source <(GOPATH=/opt/go)' >>~/.bashrc
+source ~/.bashrc
+clear 
+echo "\$GOPATH variable is set to ${GOPATH}"
--- kubernetes-operators/step1.md
+++ kubernetes-operators/step1.md
@@ -1,1 +1,19 @@
 step1
+
+This lab started a fresh Kubernetes cluster for you. Verify that it's ready:
+
+```
+{ clear && \
+  echo -e "\n=== Kubernetes Status ===\n" && \
+  kubectl get --raw '/healthz?verbose' && \
+  kubectl version --short && \
+  kubectl get nodes && \
+  kubectl cluster-info; 
+} | grep -z 'Ready\| ok\|passed\|running'
+```{{exec}}
+
+The Helm package manager used for installing applications on Kubernetes is also available:
+
+```
+helm version --short
+```{{exec}}
